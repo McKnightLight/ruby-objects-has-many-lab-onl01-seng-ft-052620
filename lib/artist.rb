ddb89987e8a90f72308d224f6774912340feecd3
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-16 14:49:53 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+  
+  attr_accessor :name, :songs 
+  
+  @@song_count = 0 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+  Song.all.select do |song|
+    song.artist == self  
+    end
+  end
+
+  def add_song(song)
+    @songs << song 
+    song.artist == self
+    @@song.count += 1 
+  end
+
+  def add_song_by_name(song_name)
+    singer = Song.new(song_name)
+    @songs << song 
+    song.artist = self 
+    @@song_count += 1 
+    add_song(singer)
+  end
+
+  def self.song_count
+    Song.all.count 
+  end
+
+end
